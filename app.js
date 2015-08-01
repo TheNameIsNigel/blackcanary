5e1c3574941bbc0bc92197bb477117472b6d1af2
drewwalton19216801 <drewwalton19216801@gmail.com>
2015-08-01 13:29:55 -0600

    Don't use bower for bootstrap libs

--- app.js
+++ app.js
@@ -1,20 +1,26 @@
-var utils = require('./lib/utils')
-  , express = require('express')
-  , app = express()
+var utils = require('./lib/utils');
 
-app.set('view engine', 'jade')
+var express = require('express');
+var http = require('http');
 
-app.get('/', function(req, res) {
+var app = express();
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+app.use(express.static('public'));
+app.use(express.static('bower_components'));
+
+app.get('/', function (req, res) {
     res.render('index'
-               , { title: 'BlackCanary Admin Portal', message: 'Hello there!'}
+        , {title: 'BlackCanary Admin Portal', message: 'Hello there!'}
     )
 })
 
-var server = app.listen(3000, function() {
+var server = app.listen(3000, function () {
     var host = server.address().address
-      , port = server.address().port
+        , port = server.address().port
 
-    if(host === '::')
+    if (host === '::')
         host = 'localhost'
     utils.log('Admin portal may be accessed at http://%s:%s', host, port)
 })
