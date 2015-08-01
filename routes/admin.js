5f0d95fe932d1bb3f23df60aa33b1680dbe37089
drewwalton19216801 <drewwalton19216801@gmail.com>
2015-08-01 15:05:36 -0600

    Basic bootstrap template

--- routes/admin.js
+++ routes/admin.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('index', { title: 'HangupsWeb' });
+    res.render('admin', { title: 'HangupsWeb Admin' });
 });
 
 module.exports = router;
