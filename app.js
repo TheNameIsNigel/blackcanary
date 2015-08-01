7ea95d4058f2fd30c8c81733d7100a925d24d3c9
drewwalton19216801 <drewwalton19216801@gmail.com>
2015-08-01 16:38:40 -0600

    views: Switch from jade to ejs

--- app.js
+++ app.js
@@ -5,15 +5,11 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
-var admin = require('./routes/admin');
-
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -24,9 +20,13 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'bower_components')));
 
-app.use('/', routes);
-app.use('/users', users);
-app.use('/admin', admin);
+app.get('/', function(req, res) {
+    res.render('pages/index');
+});
+
+app.get('/admin', function(req, res) {
+    res.render('pages/admin')
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -42,7 +42,7 @@ app.use(function(req, res, next) {
 if (app.get('env') === 'development') {
     app.use(function(err, req, res, next) {
         res.status(err.status || 500);
-        res.render('error', {
+        res.render('pages/error', {
             message: err.message,
             error: err
         });
@@ -53,7 +53,7 @@ if (app.get('env') === 'development') {
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
     res.status(err.status || 500);
-    res.render('error', {
+    res.render('pages/error', {
         message: err.message,
         error: {}
     });
